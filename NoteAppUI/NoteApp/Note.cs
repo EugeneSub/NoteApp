ea1362a1e1d9c689ccf2d6fd80e4e9fdb797fa9e
EugeneSub <KiparisL@rambler.ru>
2019-10-24 19:04:31 +0700

    Был создан новый проект NoteApp.

---
+++ NoteAppUI/NoteApp/Note.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    public class Note
+    {
+        public string name { get; set; }
+        public string category { get; set; }
+        public string text { get; set; }
+        private DateTime created { get; set; }
+        private DateTime changed { get; set; }
+    }
+
+    enum NoteCategory
+    {
+        Work,
+        Home,
+        Health,
+        People,
+        Documents,
+        Phinances,
+        Misc
+    }
+
+    public class Project
+    {
+
+    }
+
+    public class ProjectManager
+    {
+
+    }
+
+}
