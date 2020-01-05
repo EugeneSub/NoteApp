a0248e0b2a3f27631b63928a91cb919fcf6d6a9e
EugeneSub <KiparisL@rambler.ru>
2020-01-05 22:07:44 +0700

    Были добавлены юнит-тесты.

---
+++ NoteAppUI/NoteApp.UnitTests/NoteTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NoteApp.UnitTests
+{
+    [TestFixture]
+    public class NoteTest
+    {
+        [Test(Description = "Позитивный тест геттера Name")]
+        public void NoteNameGet_CorrectValue()
+        {
+            var expected = "Заголовок";
+            var note = new Note("", "", TheCategory.Documents, DateTime.Now, DateTime.Now);
+            note.Name = expected;
+            var actual = note.Name;
+            Assert.AreEqual(expected, actual, "Геттер Name возвращает неправильный заголовок");
+        }
+
+        [Test(Description = "Позитивный тест сеттера Name")]
+        public void NoteNameSet_CorrectValue()
+        {
+            var expected = "Заголовок_книжки";
+            var note = new Note("Заголовок_книжки", "", TheCategory.Documents, DateTime.Now, DateTime.Now);
+            note.Name = expected;
+            Assert.AreEqual(expected, note.Name, "Сеттер Name устанавливает неправильное значение");
+        }
+
+        [Test(Description = "Негативный тест сеттера Name, присваивение более 50 значений")]
+        public void NoteNameSet_InCorrectValue()
+        {
+            var wrongName = "000000000000000000000000000000000000000000000000000";
+            var note = new Note("", "", TheCategory.Documents, DateTime.Now, DateTime.Now);
+            Assert.Throws<ArgumentException>(() => { note.Name = wrongName; }, "-");
+        }
+
+        [Test(Description = "Позитивный тест сеттера Text")]
+        public void TitleTextSet_CorrectValue()
+        {
+            var expected = "Заголовок_книжки";
+            var note = new Note("Заголовок_книжки", "Текст", TheCategory.Documents, DateTime.Now, DateTime.Now);
+            note.Text = expected;
+            Assert.AreEqual(expected, note.Text, "Сеттер Text устанавливает неправильное значение");
+        }
+
+        [Test(Description = "Позитивный тест сеттера Category")]
+        public void NoteCategorySet_CorrectValue()
+        {
+            var expected = TheCategory.Misc;
+            var note = new Note("Заголовок_книжки", "Текст", TheCategory.Documents, DateTime.Now, DateTime.Now);
+            note.theCategory = TheCategory.Work;
+            note.theCategory = expected;
+            Assert.AreEqual(expected, note.theCategory, "Сеттер TheCategory устанавливает неправильное значение");
+        }
+
+
+    }
+}
---
+++ NoteAppUI/NoteApp.UnitTests/ProjectManagerTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+using NoteApp;
+
+
+namespace NoteApp.UnitTests
+{
+    [TestFixture]
+    class ProjectManagerTest
+    {
+        private Project _testproject1 = new Project();
+        private Project _testproject2 = new Project();
+
+        [Test(Description = "Тест сериализации")]
+        public void SaveToFile()
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string testFilePath = $@"{path}\test.json";
+            string expected = File.ReadAllText(testFilePath);
+            Note note1 = new Note("Name", "Text", TheCategory.Misc, new DateTime(2020, 01, 05).Date, new DateTime(2020, 01, 05).Date);
+            Note note2 = new Note("Name2", "Text2", TheCategory.Home, new DateTime(2020, 01, 05).Date, new DateTime(2020, 01, 05).Date);
+            Note note3 = new Note("Name3", "Text3", TheCategory.Work, new DateTime(2020, 01, 05).Date, new DateTime(2020, 01, 05).Date);
+            string filename = $@"{path}\test1.json";
+            _testproject1.Note1.Add(note1);
+            _testproject1.Note1.Add(note2);
+            _testproject1.Note1.Add(note3);
+            ProjectManager.SaveToFile(_testproject1, filename);
+            string actual = File.ReadAllText(filename);
+            Assert.AreEqual(expected, actual, "Файлы в сериализации различаются");
+        }
+
+        [Test(Description = "Тест десериализации")]
+        public Note LoadFromFile()
+        {
+            Note note1 = new Note("Name", "Text", TheCategory.Misc, new DateTime(2020, 01, 05).Date, new DateTime(2020, 01, 05).Date);
+            Note note2 = new Note("Name2", "Text2", TheCategory.Home, new DateTime(2020, 01, 05).Date, new DateTime(2020, 01, 05).Date);
+            Note note3 = new Note("Name3", "Text3", TheCategory.Work, new DateTime(2020, 01, 05).Date, new DateTime(2020, 01, 05).Date);
+            _testproject1.Note1.Add(note1);
+            _testproject1.Note1.Add(note2);
+            _testproject1.Note1.Add(note3);
+            Project expected = _testproject2;
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string testFilePath = $@"{path}\test.json";
+            Project preactual = ProjectManager.LoadFromFile(testFilePath);
+            Project actual = preactual;
+            int i = 0;
+            foreach (var Note in _testproject2.Note1)
+            {
+                Assert.AreEqual(expected.Note1[i].Name, actual.Note1[i].Name, "Значения в десериализации различаются");
+                Assert.AreEqual(expected.Note1[i].Text, actual.Note1[i].Text, "Значения в десериализации различаются");
+                Assert.AreEqual(expected.Note1[i].theCategory, actual.Note1[i].theCategory, "Значения в десериализации различаются");
+                Assert.AreEqual(expected.Note1[i].Created, actual.Note1[i].Created, "Значения в десериализации различаются");
+                Assert.AreEqual(expected.Note1[i].Changed, actual.Note1[i].Changed, "Значения в десериализации различаются");
+                i++;
+            }
+        }
+    }
+}
